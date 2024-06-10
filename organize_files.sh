060a26c8c3bcd6bcb3797dccc0bc3dcf6e2d80d7
marvol0 <murithimarvin09@gmail.com>
2024-06-10 09:37:32 +0300

    chore: create an array to define file categories

--- organize_files.sh
+++ organize_files.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+declare -A FILE_TYPES
+FILE_TYPES=( ["jpg"]="Images" ["jpeg"]="Images" ["png"]="Images" ["gif"]="Images" ["bmp"]="Images"
+             ["txt"]="Documents" ["pdf"]="Documents" ["doc"]="Documents" ["docx"]="Documents"
+             ["xls"]="Spreadsheets" ["xlsx"]="Spreadsheets"
+             ["mp3"]="Music" ["wav"]="Music" ["flac"]="Music"
+             ["mp4"]="Videos" ["mkv"]="Videos" ["avi"]="Videos" )
+
 if ["$#" -lt 1]; then
     echo "Usage: $0 directory_path [another_directory ...]"
     exit 1
 fi
+
